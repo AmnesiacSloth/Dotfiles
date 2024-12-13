51416ed66ee8c1d3ecb2e26a8531ff79cb3b1882
Christian V <amnesiacsloth@gmail.com>
2024-12-12 23:25:25 -0800

    simple bash script to create symlinks in git bash

---
+++ createSymLink.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+echo "Please select which symlink to create for your windows system"
+echo "1. neovim"
+
+echo "2. boi"
+read variable
+case $variable in
+	1)
+		ln -s "$HOME/Documents/githubRepos/Dotfiles/nvim" "$HOME/AppData/Local/nvim";; 
+	2)
+		echo "boi";;
+esac
