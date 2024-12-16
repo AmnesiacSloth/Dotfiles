5756673613664d69c2cd8c624aca9e6ab535497f
Christian V <amnesiacsloth@gmail.com>
2024-12-16 10:22:01 -0800

    testing push

--- nvim/init.lua
+++ nvim/init.lua
@@ -1,2 +1,7 @@
 require("megatherium")
+vim.opt.shell ="C:/Program Files/Git/usr/bin/bash.exe"-- Set bash.exe as the shell
+vim.opt.shellcmdflag = "-c" -- Pass commands properly to bash
+-- vim.opt.shellquote = "\"" -- Quote the shell command
+--vim.opt.shellxquote = "" -- Avoid additional escaping
+
 print("hello")
